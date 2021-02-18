24bd10323fe0e87e3be3149007dcfab3e31a128a
adriiancorral <adriancorralruiz2000@gmail.com>
2021-02-18 15:42:17 +0100

    TestRead y EditableBuffer

---
+++ EditableBufferedReader.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class EditableBufferedReader extends BufferedReader {
+
+    public EditableBufferedReader(Reader in) {
+        super(in);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
