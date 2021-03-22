05288021e1f781e1bf3a198a7fe38e8492651ee7
Adrian Corral Ruiz <adriancorralruiz@Air-de-Adrian.home>
2021-03-22 10:16:20 +0100

    legibilidad

--- practica1/src2/Console.java
+++ practica1/src2/Console.java
@@ -13,18 +13,22 @@ public class Console implements Observer {
 
     @Override
     public void update(Observable o, Object arg) {
+
         int moves = 0;
+        int col = ((Line) o).getActualColum();
+        int buffSize = ((Line) o).getBuff().size();
+
         switch ((int)arg) {
             case Constants.UP:
                 // En desarrollo
                 System.out.print(Constants.ESC_UP);
-                ((Line) o).setActualColum(((Line) o).getActualColum() - maxCols);
+                ((Line) o).setActualColum(col - maxCols);
                 break;
             
             case Constants.DOWN:
                 // En desarrollo
                 System.out.print(Constants.ESC_DOWN);
-                ((Line) o).setActualColum(((Line) o).getActualColum() + maxCols);
+                ((Line) o).setActualColum(col + maxCols);
                 break;
             
             case Constants.RIGHT:
@@ -36,11 +40,12 @@ public class Console implements Observer {
                 break;
             
             case Constants.DEL:
-                // Update terminal
-                for (int i = 0; i + ((Line) o).getActualColum() < ((Line) o).getBuff().size(); i++) {
-                    System.out.print(((Line) o).getBuff().get(i + ((Line) o).getActualColum()));
+                // Update terminal (characters that are at the right)
+                for (int i = 0; i + col < buffSize; i++) {
+                    System.out.print(((Line) o).getBuff().get(i + col));
                     moves++;
                 }
+                // Put one space at the last character
                 System.out.print(" ");
                 moves++;
                 // Put the cursor to the original position
@@ -50,12 +55,15 @@ public class Console implements Observer {
                 break;
             
             case Constants.BACKSPACE:
+                // First go one position left
                 System.out.print(Constants.ESC_LEFT);
-                // Update terminal
-                for (int i = 0; i + ((Line) o).getActualColum() < ((Line) o).getBuff().size(); i++) {
-                    System.out.print(((Line) o).getBuff().get(i + ((Line) o).getActualColum()));
+                // Update terminal (characters that are at the right)
+                for (int i = 0; i + col < buffSize; i++) {
+                    char character = ((Line) o).getBuff().get(i + col);
+                    System.out.print(character);
                     moves++;
                 }
+                // Put one space at the last character
                 System.out.print(" ");
                 moves++;
                 // Put the cursor to the original position
@@ -65,10 +73,12 @@ public class Console implements Observer {
                 break;
             
             default:    // Default is addChar()
+                // Write the character on the cursor position
                 System.out.print(((Line) o).getBuff().get((int) arg));
-                // Update terminal
-                for (int i = 0; i + ((Line) o).getActualColum() < ((Line) o).getBuff().size(); i++) {
-                    System.out.print(((Line) o).getBuff().get(i + ((Line) o).getActualColum()));
+                // Update terminal (characters that are at the right)
+                for (int i = 0; i + col < buffSize; i++) {
+                    char character = ((Line) o).getBuff().get(i + col);
+                    System.out.print(character);
                     moves++;
                 }
                 // Put the cursor to the original position
